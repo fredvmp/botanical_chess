59692efb230f94b4d28b68f20ce6905a844d4ded
Fred <alfredotortuero@gmail.com>
2025-05-22 20:02:21 +0200

    Organización pantalla

---
+++ botanical_chess/src/components/Chessboard.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./Chessboard.css";
+
+const horizontalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
+const verticalAxis = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+
+
+
+const Chessboard = () => {
+
+    return (
+        <>
+            <div id='chessboard'>
+                <h1>Chessboard</h1>
+
+
+            </div>
+        </>
+    )
+}
+
+
+export default Chessboard;
+
+
+
+
